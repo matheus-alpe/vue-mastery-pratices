eac3846af99f0adcbc9c5d10c2cb9395f2ccc2c6
Matheus Alves Pereira <matttalves@gmail.com>
2023-04-16 19:07:02 -0300

    refactor: use yup

--- src/views/ComponentForm.vue
+++ src/views/ComponentForm.vue
@@ -75,6 +75,7 @@
 
 <script>
 import { useForm, useField } from 'vee-validate'
+import { object, string, number, boolean } from 'yup'
 
 export default {
   data() {
@@ -92,41 +93,15 @@ export default {
   },
 
   setup() {
-    const required = (value) => {
-      const requiredMessage = 'This field is required'
-      if (value === undefined || value === null) return requiredMessage
-      if (!String(value).length) return requiredMessage
-
-      return true
-    }
-
-    const minLength = (number, value) => {
-      if (String(value).length < number) return 'Please type at least ' + number + ' characters'
-
-      return true
-    }
-
-    const anything = () => {
-      return true
-    }
-
-    const validationSchema = {
-      category: required,
-      title: (value) => {
-        const requiredResponse = required(value)
-        if (requiredResponse !== true) return requiredResponse
-
-        const hasMinLength = minLength(3, value)
-        if (hasMinLength !== true) return hasMinLength
-
-        return true
-      },
-      description: required,
-      location: undefined,
-      pet: anything,
-      catering: anything,
-      music: anything
-    }
+    const validationSchema = object({
+      category: string().required(),
+      title: string().required('A cool title is required').min(3),
+      description: string().required(),
+      location: string(),
+      pets: number(),
+      catering: boolean(),
+      music: boolean()
+    })
 
     const { handleSubmit, errors } = useForm({
       validationSchema,
