819ae8b6cf14341d8a42aa9ab95c45db98648f27
Matheus Alves Pereira <matttalves@gmail.com>
2023-04-16 16:51:33 -0300

    feat: vee-validate

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -3,11 +3,15 @@
     <BaseInput
       label="Email"
       type="email"
+      v-model="email"
+      :error="emailError"
     />
 
     <BaseInput
       label="Password"
       type="password"
+      v-model="password"
+      :error="passwordError"
     />
 
     <BaseButton
@@ -20,14 +24,52 @@
 </template>
 
 <script>
+import { useField, useForm } from 'vee-validate'
+
 export default {
   setup() {
     function onSubmit() {
       alert('Submitted')
     }
 
+    const requiredMessage = 'This field is required'
+
+    const validations = {
+      email: (value) => {
+        console.log('email', value)
+
+        if (!value) return requiredMessage
+
+        const validateEmail =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if (!validateEmail.test(String(value).toLowerCase())) {
+          return 'Please enter a valid email address'
+        }
+
+        return true
+      },
+      password: (value) => {
+        console.log('password', value)
+        if (value === undefined || value === null) return requiredMessage
+        if (!String(value).length) return requiredMessage
+        return true
+      }
+    }
+
+    useForm({
+      validationSchema: validations
+    })
+
+    const email = useField('email')
+    const password = useField('password')
+
     return {
-      onSubmit
+      onSubmit,
+      email: email.value,
+      emailError: email.errorMessage,
+      password: password.value,
+      passwordError: password.errorMessage
     }
   }
 }
