1728bd60d983ad024863a470dfb84f62f0587404
Matheus Alves Pereira <matttalves@gmail.com>
2023-04-17 21:28:35 -0300

    init

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/matheus-alpe/workspace/personal-projects/vue-mastery-pratices/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
