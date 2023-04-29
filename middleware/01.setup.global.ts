74cbdde62c28296ce214612964c19ac4425ef74a
Matheus Alves Pereira <matttalves@gmail.com>
2023-04-28 23:23:23 -0300

    feat: handling errors on middleware

--- middleware/01.setup.global.ts
+++ middleware/01.setup.global.ts
@@ -1,3 +1,19 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('--- Setup Global Middleware ---')
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log('--- Error Handler Global Middleware ---')
+
+  // if (to.path === '/login') {
+  //   throw createError({
+  //     statusCode: 405,
+  //     statusMessage: 'Oh snap it broke!'
+  //   })
+  // }
+
+  // return abortNavigation('Failed at global middleware')
+
+  // try {
+  //   const data = await fetch('/api/this-path')
+  //   return navigateTo('profile', data)
+  // } catch (error) {
+  //   return abortNavigation(error)
+  // }
 })
