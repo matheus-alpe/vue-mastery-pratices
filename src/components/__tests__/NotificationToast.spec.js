f048ba0a20dc98ff5f6f4ef0491f820aeae2df05
Matt <matttalves@gmail.com>
2023-11-12 11:30:52 -0300

    feat: NotificationToast and test

---
+++ src/components/__tests__/NotificationToast.spec.js
@@ -0,0 +1,64 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import NotificationToast from '../NotificationToast.vue'
+
+describe('NotificationToast', () => {
+
+  it('renders the correct style for error', () => {
+    const status = 'error'
+    const wrapper = mount(NotificationToast, {
+      props: {
+        status,
+      }
+    })
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['notification--error']))
+  })
+
+  it('renders the correct style for success', () => {
+    const status = 'success'
+    const wrapper = mount(NotificationToast, {
+      props: {
+        status,
+      }
+    })
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['notification--success']))
+  })
+
+  it('renders the correct style for info', () => {
+    const status = 'info'
+    const wrapper = mount(NotificationToast, {
+      props: {
+        status,
+      }
+    })
+
+    expect(wrapper.classes()).toEqual(expect.arrayContaining(['notification--info']))
+  })
+
+  it('notification slies up when message is empty', () => {
+    const message = ''
+    const wrapper = mount(NotificationToast, {
+      props: { message },
+    })
+
+    expect(wrapper.classes('notification--slide')).toBe(false)
+  })
+
+  it('emits event when close button is clicked', async () => {
+    const wrapper = mount(NotificationToast)
+    const closeButton = wrapper.find('button')
+    await closeButton.trigger('click')
+
+    expect(wrapper.emitted()).toHaveProperty('clear-notification')
+  })
+
+  it('renders correct message to viewer', () => {
+    const message = 'Something happened, try again'
+    const wrapper = mount(NotificationToast, { props: { message } })
+
+    expect(wrapper.find('.notification__text').text()).toBe(message)
+  })
+})
