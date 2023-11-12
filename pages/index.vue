30e7a99aa7a420c70ed65a8519393929b2d9e677
Matt <matttalves@gmail.com>
2023-11-12 16:24:31 -0300

    feat: handling errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <script setup>
-const { data } = await useFetch('/api/pikachu')
+const { data, error } = await useFetch('/api/asdsa')
 </script>
 
 <template>
   <h1>Home page for nuxt 3</h1>
 
-  <div>
+  <div v-if="data">
       <img :src="data.sprite"/>
       <h2>{{ data.id }}: {{ data.name }}</h2>
   </div>
+
+  <p v-else>{{ error }}</p>
 </template>
