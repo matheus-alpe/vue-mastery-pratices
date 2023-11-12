aeda197b6857f03fee88bce0b8b666fc283bd299
Matt <matttalves@gmail.com>
2023-11-12 16:18:11 -0300

    feat: dynamic server param

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+const { data } = await useFetch('/api/pikachu')
+</script>
+
 <template>
   <h1>Home page for nuxt 3</h1>
+
+  <div>
+      <img :src="data.sprite"/>
+      <h2>{{ data.id }}: {{ data.name }}</h2>
+  </div>
 </template>
