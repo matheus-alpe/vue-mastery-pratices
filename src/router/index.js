4607a12f0a77f1b280fbc133452aea50f969e786
Matt <matttalves@gmail.com>
2023-11-12 14:04:25 -0300

    end

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TextView from '../views/TextView.vue'
+import AudioView from '../views/AudioView.vue'
+import ImageView from '../views/ImageView.vue'
+import { useTokenizeStore } from '../stores/tokenize'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/text',
+      name: 'text',
+      component: TextView
+    },
+    {
+      path: '/audio',
+      name: 'audio',
+      component: AudioView
+    },
+    {
+      path: '/image',
+      name: 'image',
+      component: ImageView
     }
   ]
 })
---
+++ src/stores/imageChat.js
@@ -0,0 +1,51 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useImageChatStore = defineStore('imageChat', () => {
+  const prompt = ref([])
+  const miniGPTResponse = ref('')
+  const question = ref('')
+  const imageURL = ref('')
+  const questionAnswerList = ref([])
+
+  function createPrompt() {
+    fetch('http://localhost:3000/replicate-chain', {
+      method: 'POST',
+      body: JSON.stringify({
+        image: imageURL.value,
+        prompt: question.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        miniGPTResponse.value = data.message
+        questionAnswerList.value.push({
+          question: question.value,
+          answer: data.message
+        })
+        question.value = ''
+      })
+  }
+
+  function clearChat() {
+    prompt.value = []
+    miniGPTResponse.value = ''
+    question.value = ''
+    imageURL.value = ''
+    questionAnswerList.value = []
+  }
+
+  return {
+    prompt,
+    miniGPTResponse,
+    createPrompt,
+    imageURL,
+    question,
+    clearChat,
+    questionAnswerList
+  }
+})
